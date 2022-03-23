b512be56af37c2c9429c12f8bd907b4c6dfafe31
Leonardo Granata <lgranata@gmail.com>
2022-03-23 16:56:11 -0300

    Separate class into its own file

---
+++ SkuVaultAssessment/FizzBuzz.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+public class FizzBuzz
+{
+    private readonly IEnumerable<int> _numbers;
+
+    public FizzBuzz(int end, int start = 0)
+    {
+        if (end < start)
+        {
+            throw new Exception(ExceptionMessages.StartMustBeLessThanEnd);
+        }
+        if (start < 0)
+        {
+            throw new Exception(ExceptionMessages.StartMustBeGreaterThanZero);
+        }
+
+        _numbers = Enumerable.Range(start + 1, end - start).ToList();
+    }
+
+    public string RunFizzBuzz()
+    {
+        StringBuilder outputString = new StringBuilder();
+
+        foreach (int i in _numbers)
+        {
+            if (i % 3 == 0 && i % 5 == 0)
+            {
+                outputString.AppendLine("FizzBuzz");
+            }
+            else if (i % 3 == 0)
+            {
+                outputString.AppendLine("Fizz");
+            }
+            else if (i % 5 == 0)
+            {
+                outputString.AppendLine("Buzz");
+            }
+            else
+            {
+                outputString.AppendLine(i.ToString());
+            }
+        }
+        return outputString.ToString();
+    }
+}
+
+public static class ExceptionMessages
+{
+    public const string StartMustBeLessThanEnd = "Start must be less than end.";
+    public const string StartMustBeGreaterThanZero = "Start must be greater than zero.";
+}
--- SkuVaultAssessment/Program.cs
+++ SkuVaultAssessment/Program.cs
@@ -22,7 +22,7 @@ namespace SkuVaultAssessment
                 Console.WriteLine("Results:");
                 try
                 {
-                    FizzBuzz fb = new FizzBuzz(endNumber, -1);
+                    FizzBuzz fb = new FizzBuzz(endNumber);
                     Console.WriteLine(fb.RunFizzBuzz());
                 }
                 catch (Exception ex)
@@ -36,55 +36,5 @@ namespace SkuVaultAssessment
             Console.ReadLine();
         }
     }
-
-    public class FizzBuzz
-    {
-        private readonly IEnumerable<int> _numbers;
-
-        public FizzBuzz(int end, int start = 0)
-        {
-            if (end < start)
-            {
-                throw new Exception(ExceptionMessages.StartMustBeLessThanEnd);
-            }
-            if (start < 0)
-            {
-                throw new Exception(ExceptionMessages.StartMustBeGreaterThanZero);
-            }
-
-            _numbers = Enumerable.Range(start + 1, end - start).ToList();
-        }
-
-        public string RunFizzBuzz()
-        {
-            StringBuilder outputString = new StringBuilder();
-
-            foreach (int i in _numbers)
-            {
-                if (i % 3 == 0 && i % 5 == 0)
-                {
-                    outputString.AppendLine("FizzBuzz");
-                }
-                else if (i % 3 == 0)
-                {
-                    outputString.AppendLine("Fizz");
-                }
-                else if (i % 5 == 0)
-                {
-                    outputString.AppendLine("Buzz");
-                }
-                else
-                {
-                    outputString.AppendLine(i.ToString());
-                }
-            }
-            return outputString.ToString();
-        }
-    }
-
-    public static class ExceptionMessages
-    {
-        public const string StartMustBeLessThanEnd = "Start must be less than end.";
-        public const string StartMustBeGreaterThanZero = "Start must be greater than zero.";
-    }
+ 
 }
