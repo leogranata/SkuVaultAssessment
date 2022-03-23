e34a8217a993f541a4b79a372ea272edb8c2fefb
Leonardo Granata <lgranata@gmail.com>
2022-03-23 16:47:11 -0300

    Add project files.

---
+++ SkuVaultAssessment/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkuVaultAssessment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+
+    public class FizzBuzz
+    {
+        private readonly IEnumerable<int> _numbers;
+        public FizzBuzz(int end, int start = 0)
+        {
+            if (start < end)
+            {
+                throw new Exception("Start must be less end.");
+            }
+            _numbers = Enumerable.Range(start, end - start).ToList();
+        }
+        public string RunFizzBuzz()
+        {
+            // TODO: add implementation here
+        }
+    }
+}
